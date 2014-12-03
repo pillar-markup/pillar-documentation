cf4c28b4649b8f09b5bae67c89a2abe7f51b1604
Damien Cassou <damien.cassou@gmail.com>
2014-12-03 15:32:36 +0100

    Move documentation to EnterprisePharo book
    https://ci.inria.fr/pharo-contribution/job/EnterprisePharoBook/lastSuccessfulBuild/artifact/PillarChap

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-./pillar export --to="HTML all in one"
-./pillar export --to="Markdown (github) all in one"
-./pillar export --to="LaTeX all in one"
-./pillar export --to="Markdown by chapter"
-./pillar prepareForGitBook --from="Markdown by chapter"
